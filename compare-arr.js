9951200a36ab599117a2a9fa940807a135efb5c3
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-01-11 15:45:12 +0100

    Add S2Base64Tree

--- compare-arr.js
+++ compare-arr.js
@@ -1,10 +1,20 @@
-module.exports = (arr1, arr2) => {
+module.exports = (arr1, arr2, orderStrict = true) => {
   if (arr1.length !== arr2.length) {
     throw `Length is not equal (${arr1.length} != ${arr2.length})`;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      throw `Line #${i + 1} is not equal (${arr1[i]} != ${arr2[i]})`;
+  if (orderStrict) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        throw `Line #${i + 1} is not equal (${arr1[i]} != ${arr2[i]})`;
+      }
     }
+  } else {
+    arr2 = arr2.map(a => a);
+    arr1.forEach(a1 => {
+      const a2idx = arr2.findIndex(a2 => a1 === a2);
+      if (a2idx === -1) {
+        throw `${a1} in array 1 is not found in array 2`;
+      }
+    });
   }
 };
---
+++ make-tree-test.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+const makeTree = require('./s2cells-tree/make-tree');
+const expandTree = require('./s2cells-tree/expand-tree');
+const encoder = require('./s2cells-tree/make-tree-encoder');
+const decoder = require('./s2cells-tree/make-tree-decoder');
+const compareArr = require('./compare-arr');
+
+const statsPath = './out/stats-b64tree.csv';
+fs.writeFileSync(statsPath, 'PRECISION,S2B64TREE_KB,S2B64TREE_TIME_SEC\n');
+
+[9, 12, 14, 17].forEach(precision => {
+  console.log(`## PRECISION ${precision} ##`);
+  let sumSizeBytes = 0;
+  let sumTimeMillisecs = 0;
+
+  const filePaths = fs.readdirSync(`./out/${precision}`)
+    .map(f => `./out/${precision}/${f}`)
+    .filter(f => path.extname(f) === '.s2cells-base64');
+  filePaths.forEach(filePath => {
+    stdout.write(`Checking ${path.basename(filePath)}...`);
+    const data = fs.readFileSync(filePath).toString().split('\n').filter(x => x);
+    const startTime = new Date().getTime();
+    const tree = makeTree(data);
+    const encodedTree = encoder.encode(tree);
+    sumTimeMillisecs += new Date().getTime() - startTime;
+    sumSizeBytes += Buffer.from(encodedTree).byteLength;
+    const decodedTree = decoder.decode(encodedTree);
+    const expandedTree = expandTree(decodedTree);
+    compareArr(data, expandedTree, false);
+    console.log('OK');
+  });
+
+  fs.appendFileSync(statsPath, `${precision},${sumSizeBytes / 1024},${sumTimeMillisecs / 1000}\n`);
+});
+
+console.log('FINISHED');
